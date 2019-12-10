d1446cba5b90864a5526d48bbe19f63d19c18112
kohei1218 <yandapanda1218@gmail.com>
2019-12-10 21:02:01 +0900

    Initial Commit

---
+++ LoveMinus/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoveMinus
+//
+//  Created by kohei saito on 2019/12/10.
+//  Copyright © 2019 kohei saito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
